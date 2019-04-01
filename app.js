25e0111a0903c766bde052d6d28b30374ba2a81a
xiaoyu <923996381@qq.com>
2019-04-01 23:39:11 +0800

    init test-gulu

---
+++ app.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import { Button,ButtonGroup,Icon} from 'xiaoyu-test-1'
+import 'xiaoyu-test-1/dist/index.css'
+
+Vue.component('g-button',Button)
+Vue.component('button-group',ButtonGroup)
+Vue.component('g-icon',Icon)
+
+new Vue({
+    el: '#app'
+})
