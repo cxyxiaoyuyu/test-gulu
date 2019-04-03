18256aa63ec0d89ee5b3642f106172e68a652c0a
xiaoyu <923996381@qq.com>
2019-04-04 01:17:17 +0800

    add input component

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 import Vue from 'vue'
-import { Button,ButtonGroup,Icon} from 'xiaoyu-test-1'
+import { Button,ButtonGroup,Icon,Input } from 'xiaoyu-test-1'
 import 'xiaoyu-test-1/dist/index.css'
 
+console.log(Button,ButtonGroup,Icon,Input)
 Vue.component('g-button',Button)
 Vue.component('button-group',ButtonGroup)
 Vue.component('g-icon',Icon)
+Vue.component('g-input',Input)
 
 new Vue({
     el: '#app',
     data: {
-        loading: true
+        loading: true,
+        message: "Hello Vue"
     }
 })
